85d4c16d0c12b20b15d10e7b24b78326ed070f41
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-04-01 18:28:49 +0530

    Create mem_decompression.c

---
+++ mem_decompression.c
@@ -0,0 +1,29 @@
+void restore_page(FILE *dump_file, char *output_page) {
+    char compressed[MAX_COMPRESSED_SIZE];
+    int compressed_size;
+
+    // Read compressed size from dump file
+    if (fread(&compressed_size, sizeof(int), 1, dump_file) != 1) {
+        perror("Error reading compressed size from dump file");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Read the compressed data into memory
+    if (fread(compressed, compressed_size, 1, dump_file) != 1) {
+        perror("Error reading compressed data from dump file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Decompress the data using LZ4
+    int decompressed_size = LZ4_decompress_safe(compressed, output_page, compressed_size, PAGE_SIZE);
+    
+    if (decompressed_size < 0) {
+        fprintf(stderr, "LZ4 decompression failed for page\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (decompressed_size != PAGE_SIZE) {
+        fprintf(stderr, "Decompressed size does not match page size\n");
+        exit(EXIT_FAILURE);
+    }
+}
